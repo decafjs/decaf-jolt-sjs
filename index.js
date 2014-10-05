603f563fff8af53696a46c302b12122d9642e624
Michael Schwartz <mykes@sportstwo.com>
2014-10-05 07:34:32 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mschwartz on 10/5/14.
+ */
+
+/*global require, exports, decaf */
+
+decaf.extend(exports, {
+    SjsFile: require('lib/SjsServer').SjsFile,
+    SjsServer: require('lib/SjsServer').SjsServer
+});
